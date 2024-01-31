8eb08014d679fb3b351ad6b686dcd111aee54f1e
FranNinaa <francinesantoss85@gmail.com>
2024-01-31 07:52:58 -0300

    :zap:perf: 002 - na mesma tarefa fiz uma melhoria para que caso de um erro mostrar de forma mais amigavel e feito implementação do cors para assim qualquer ip possa acessar o projeto

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,26 @@
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
 import { router } from './routes';
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        //se for uma instancia do tipo erro
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+    return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+})
 
 app.listen(4444, () => console.log('Server is running on port 4444'));
